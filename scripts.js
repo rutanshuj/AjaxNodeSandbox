c04630cb10cd739f6f02164bf4f8d0083ebc9169
rutanshuj <rjhaveri41@gmail.com>
2018-04-14 16:44:08 +0530

    Bug resolved

--- scripts.js
+++ scripts.js
@@ -1,47 +1,28 @@
 $(function () {
-    // GET/READ command
-    $('#get-button').on('click', function () {
 
+    //GET/READ Command
+
+    $('#get-button').on('click', function () {
         $.ajax({
             url: '/products',
             contentType: 'application/json',
-            success: function(response){
-                console.log("Get Products");
-                var tbodyEl = $('tbody');
-                tbodyEl.html('');
+            success: function (response) {
+                var tbodyEL = $('tbody');
+                tbodyEL.html('');
                 response.products.forEach(function(product){
-                   tbodyEl.append('\
+                   tbodyEL.append('\
                    <tr>\
-                   <td class ="id">' + product.id  + '</td>\
-                   <td><input type ="text" class="name" value="'+product.name+'"></td>\
-                   <td>\
-                   <button class="update-button">\
-                   UPDATE/PUT </button>\
-                   </button>\
-                   <button class ="delete-button">\
-                   DELETE\</button>\
+                   <td class ="id">'+product.id+'</td>\
+                   <td><input type="text" class="name" value="'+product.name+'"></td>\
+                   <td><button class="update">\
+                   UPDATE/PUT</button>\
                    </td>\
+                   <td><button class="delete-button">\
+                   DELETE</button></td>\
                    </tr>\
                    ');
                 });
             }
         })
-    });
-
-    //CREATE/POST
-    $('#create-form').on('submit', function (event) {
-        event.preventDefault();
-        var createInput = $('#createInput');
-        $.ajax({
-            url: '/products',
-            method: 'POST', //Default is GET, hence we didn't mention it before
-            contentType: 'application/json',
-            data: JSON.stringify({name: createInput.val()}),
-            success: function(response){
-                console.log(response);
-                createInput.val(' ');
-                $('#get-button').click();
-            }
-        });
     });
 });
--- server.js
+++ server.js
@@ -1,36 +1,27 @@
+//CRUD - Create(POST), Retrieve (GET), Update(PUT), Delete(DELETE)
+
 const express = require('express');
 const app = express();
-const bodyParser = require('body-parser');
 var products =[
     {
-        id: 1,
-        name: 'laptop'
+        id:1,
+        name: "watch"
     },
     {
         id: 2,
-        name: 'microwave'
+        name: "microwave"
     }
 ];
-
-var currentID = 2;
+var currentId = 2;
 var PORT = process.env.PORT || 3000;
+
 app.use(express.static(__dirname));
-app.use(bodyParser.json);
-app.get('/products', function(req, res){
-    console.log(req);
-   res.send({
-       products: products
-   });
-});
-app.post('/products', function(req, res){
-    var productName = req.body.name;
-    currentID++;
-    products.push({
-        id: currentID,
-        name: productName
+
+app.get('/products', function (req, res) {
+    res.send({
+        products: products
     });
-    res.send("Successfully created product");
 });
 app.listen(PORT, function(){
-    console.log('Server listening to ' + PORT);
+    console.log('Server listening on '+PORT);
 });
